7cdf7ef367de845509adf404d5b4e5e5527fa316
suhas <suhas.wadadekar@gmail.com>
2017-11-03 00:27:35 -0400

    Initial commit

---
+++ src/main/java/scorer/BowlingScorer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package scorer;
+
+/**
+ * @author suhas
+ *
+ */
+public class BowlingScorer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
