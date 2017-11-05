f3b7b8572beb68bd1b0d701e46f95d869efdf137
suhas <suhas.wadadekar@gmail.com>
2017-11-05 00:44:35 -0400

    Improved testing of frame creation

--- src/test/java/bowling/GameTest.java
+++ src/test/java/bowling/GameTest.java
@@ -2,6 +2,9 @@ package bowling;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,13 +17,30 @@ public class GameTest {
 	@Test
 	public void testSetRollSequence_AllMisses() {
 		Game game = new Game("--------------------");
-		assertEquals(10, game.getFrames().size());
+		LinkedList<BowlingFrame> frames = game.getFrames();
+		assertEquals(10, frames.size());
+		for (BowlingFrame frame : frames) {
+			List<Roll> rolls = frame.getRolls();
+			assertEquals(2, rolls.size());
+			assertEquals(Roll.MISS, rolls.get(0));
+			assertEquals(Roll.MISS, rolls.get(1));
+		}
 	}
 
 	@Test
 	public void testSetRollSequence_AllStrikes() {
 		Game game = new Game("XXXXXXXXXXXX");
-		assertEquals(10, game.getFrames().size());
+		LinkedList<BowlingFrame> frames = game.getFrames();
+		assertEquals(10, frames.size());
+		for (int count = 0; count < frames.size() - 1; count++) {
+			List<Roll> rolls = frames.get(0).getRolls();
+			assertEquals(1, rolls.size());
+			assertEquals(Roll.STRIKE, rolls.get(0));
+		}
+		assertEquals(frames.getLast().getRolls().size(), 3);
+		assertEquals(Roll.STRIKE, frames.getLast().getRolls().get(0));
+		assertEquals(Roll.STRIKE, frames.getLast().getRolls().get(1));
+		assertEquals(Roll.STRIKE, frames.getLast().getRolls().get(2));
 	}
 
 }
